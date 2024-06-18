97cda8239dca480092ead53dadefa577493ba629
nnathanielleinahtann <nnathanielleinahtann@gmail.com>
2024-06-18 19:14:16 +0000

    bye

--- next.config.js
+++
@@ -1,8 +0,0 @@
-  /** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: "/toltrackers",
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
-  };
-  
-  module.exports = nextConfig;
